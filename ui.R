28dbb2e959c75fa0efc97784b85b7665837d512a
Jacob Cecile <jacobcecile@gmail.com>
2014-03-20 17:20:58 -0400

    Basically complete dummy UI.

--- ui.R
+++ ui.R
@@ -8,79 +8,71 @@
 # Libraries ####
 library(shiny)
 
-
 # UI ####
 shinyUI(pageWithSidebar(
   
   # Heading
   headerPanel("crossdateR"),
-  
-  # Tab 1
-  
+    
   # Sidebar
   sidebarPanel(
     # Standardization options
-    
-    # Model (check)
     wellPanel(
+      h3("Standardization options"),
       
-      h4("Model"),
+      # Standardization model
+      checkboxGroupInput("model", h4("Model effects"),
+                         c("Tree", "Time", "Age"),
+                         selected=c("Time", "Age")),
       
-      checkboxInput(inputId = "modelTree",
-                    label = "Tree",
-                    value = FALSE),
-      checkboxInput(inputId = "modelTime",
-                    label = "Time",
-                    value = TRUE),      
-      checkboxInput(inputId = "modelAge",
-                     label = "Age",
-                     value = TRUE)
+
+      
+      # Model fitting optimizer
+      selectInput(inputId = "optim",
+                  label = h4("Optimizer"),
+                  choices = c("Sequential" = "sequential",
+                              "Alternate" = "alternate",
+                              "GLM" = "glm",
+                              "GAM" = "gam"),
+                  selected = "alternate"
+      ),
+      
+      # Clustering
+      h3("Clustering"),
+      
+      #Split
+      # Only appears if age in model
+      checkboxInput("split_age", label="Use multiple split age effects"),
+      
+      # Automatic clustering
+      # Only appears if split age
+      checkboxInput("auto_cluster", label="Identify clusters automatically"),
+      
+      # Select number of clusters
+      # Only displays when automatic clustering is enabled 
+      # Max is number of series
+      # 0 means automatic number of clusters
+      numericInput("num_clusters", strong("Number of clusters"), min=0, value=2)
       
     ),
-    
-    #Split (check)
-    wellPanel(
-      
-      h4("Split"),
-      
-      checkboxInput(inputId = "splitTree",
-                    label = "Tree",
-                    value = FALSE),
-      checkboxInput(inputId = "splitTime",
-                    label = "Time",
-                    value = FALSE),      
-      checkboxInput(inputId = "splitAge",
-                    label = "Age",
-                    value = TRUE)
-      
-    ),
-    
-    # optim(drop)
-    selectInput(inputId = "optim",
-                label = h4("Optimizer"),
-                choices = c("Sequential" = "sequential",
-                            "Alternate" = "alternate",
-                            "GLM" = "glm",
-                            "GAM" = "gam"),
-                selected = "Alternate"
-    ),
-    
     
     # IO
     wellPanel(
-      h4("Load and save"),
+      h3("Data"),
       # File input
+      h4("Load data"),
       fileInput("files", "Upload raw tree ring data", multiple=TRUE),
       
       # Download results
       # TRA, change list, final standardization w/ data
-      downloadButton("new_tra", "Updated dataset"),
+      h4("Save data"),
+      downloadButton("new_tra", "Download updated dataset"),
       
       downloadButton("change_list", "Download change list"),
       
       downloadButton("last_standardization", "Download last standardization")
       
-      ),
+    ),
     
     # Standardize! (button)
     actionButton("standardize", "Standardize my data")
@@ -89,34 +81,88 @@ shinyUI(pageWithSidebar(
   
   
   mainPanel(
-    # Tab 1
-    # All standardization graphs
-    
-    # Model fit stats
-    # R^2, adj. R^2, sigma, AIC, BIC
-    
-    # Tab 2
-    # List of series (selectable, maybe scrollable)
-    # Beside them average residuals etc.
-    # Check box for "fixed" vs. "floating"
-    
-    # Tab 3
-    # Graph of residuals by time for selected series
-    # Can click on a point to select
-    
-    # Shift year +/-
-    numericInput("offset", strong("Shift series"), value=0),
-    
-    # Merge rings
-    actionButton("merge", "Merge ring with next"),
-    
-    # Split ring
-    actionButton("split", "Split ring into two")
-    
+    tabsetPanel(
+      
+      # Tab 1
+      tabPanel(strong("Standardization"), 
+        # All standardization plot
+        # Select plot using dropdown menu
+        # Should change options given model
+        selectInput("std_plot_to_show", label=strong("Plot to show"),
+                    choices=c("Tree effect", 
+                              "Time effect", 
+                              "Age effect",
+                              "Residuals density"),
+                    selected="Time effect"  
+        ),
+        
+        plotOutput("standardization_plot"),
+       
+        # Model fit stats
+        # R^2, adj. R^2, sigma, AIC, BIC
+        wellPanel(
+          h3("Model fit"),
+          tableOutput("model_fit")
+        )
+      ),
+      
+      # Tab 2
+      tabPanel(strong("Series"),
+
+        # observe() + updateCheckBoxGroupInput() to populate list of series               
+        
+        # Hierarchical cluster plot of series
+        # ggdendro library
+        plotOutput("hclust_series_plot"),
+        
+        # Summary for series
+        # Sortable table: http://glimmer.rstudio.com/szakacs/FooTableDemo/
+        # Name
+        # Start year, end year
+        # Tree effect (if applicable)
+        # Mean value, standard deviation of residuals
+        tableOutput("series_summary"),
+        
+        # List of series to include in chronology
+        # Should be merged into summary table
+        # https://groups.google.com/forum/#!topic/shiny-discuss/38Edf85wl_g
+        checkboxGroupInput("inc_series", h4("Series to include in chronology"),
+                           c("T1",
+                             "T2")
+        )
+      ),
+      
+      # Tab 3
+      tabPanel(strong("Crossdating"), 
+        
+        # Select series to crossdate
+        selectInput("crossdate_series", label=strong("Series to crossdate"),
+                    choices=c("T1", "T2")),
+        
+        # Graph of residuals by time for selected series
+        # Can click on a point to select
+        # https://gist.github.com/trestletech/5929598
+        plotOutput("crossdate_plot"),
+               
+        # Shift year +/-
+        numericInput("offset", strong("Shift series"), value=0),
+       
+        # Automatic shifting
+        # Pressing it again moves to next best position
+        actionButton("auto_offset", "Shift series automatically"),
+       
+        # Select year
+        numericInput("selectedYear", strong("Select Year"), value=2000),
+        
+        # Merge rings
+        actionButton("merge", "Merge ring with next"),
+       
+        # Split ring
+        actionButton("split", "Split ring into two"),
+       
+        # Reset series
+        actionButton("reset", "Reset series")
+      )
+    )
   )
-  
-  
-  
-  
-  
 ))
