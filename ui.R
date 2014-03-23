d3a41fb57dea045b6300cebb833da0535a373ce4
Alice Cecile <alice.i.cecile@gmail.com>
2014-03-23 14:52:08 -0400

    Added lorem ipsum for "About" tab.

--- ui.R
+++ ui.R
@@ -198,7 +198,16 @@ shinyUI(pageWithSidebar(
       ),
       
       # Tab 6
-      tabPanel(strong("About")
+      tabPanel(strong("About"),
+        
+               h1("Theory"),
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        
+        h1("Help"),
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        
+        h1("Author"),
+        "Alice I. Cecile, 2014"
       )       
     )
   )
